03ffe202a3a6c2c3b57e400eeaf84e24c075ba3a
Hans Bakker <h.bakker@antwebsystems.com>
2024-11-07 17:03:33 +0700

    fix: correction on last commit

--- moqui/disableSeca.sh
+++ moqui/disableSeca.sh
@@ -2,6 +2,6 @@
 mv runtime/component/mantle-usl/service/AccountingInvoice.secas.xml runtime/component/mantle-usl/service/AccountingInvoice.secas1.xml
 mv runtime/component/mantle-usl/service/AccountingLedger.secas.xml runtime/component/mantle-usl/service/AccountingLedger.secas1.xml
 mv runtime/component/mantle-usl/service/AccountingPayment.secas.xml runtime/component/mantle-usl/service/AccountingPayment.secas1.xml
-mv runtime/component/mantle-usl/service/ProductAssetPayment.secas.xml runtime/component/mantle-usl/service/ProductAssetPayment.secas1.xml
+mv runtime/component/mantle-usl/service/ProductAsset.secas.xml runtime/component/mantle-usl/service/ProductAsset.secas1.xml
 mv runtime/component/mantle-usl/service/ProductSubscription.secas.xml runtime/component/mantle-usl/service/ProductSubscription.secas1.xml 
  
--- moqui/enableSeca.sh
+++ moqui/enableSeca.sh
@@ -2,5 +2,5 @@
 mv runtime/component/mantle-usl/service/AccountingInvoice.secas1.xml runtime/component/mantle-usl/service/AccountingInvoice.secas.xml
 mv runtime/component/mantle-usl/service/AccountingLedger.secas1.xml runtime/component/mantle-usl/service/AccountingLedger.secas.xml
 mv runtime/component/mantle-usl/service/AccountingPayment.secas1.xml runtime/component/mantle-usl/service/AccountingPayment.secas.xml
-mv runtime/component/mantle-usl/service/ProductAssetPayment.secas1.xml runtime/component/mantle-usl/service/ProductAssetPayment.secas.xml
+mv runtime/component/mantle-usl/service/ProductAsset.secas1.xml runtime/component/mantle-usl/service/ProductAsset.secas.xml
 mv runtime/component/mantle-usl/service/ProductSubscription.secas1.xml runtime/component/mantle-usl/service/ProductSubscription.secas.xml 
