80b557db170b813c86f7a8946eeae4abbd912c9c
Hans Bakker <h.bakker@antwebsystems.com>
2025-09-29 12:31:01 +0700

    feat: more opportunity translations

--- flutter/packages/growerp_marketing/lib/src/opportunities/widgets/opportunity_list_table_def.dart
+++ flutter/packages/growerp_marketing/lib/src/opportunities/widgets/opportunity_list_table_def.dart
@@ -21,14 +21,20 @@ import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 import 'package:growerp_marketing/l10n/generated/marketing_localizations.dart';
 import '../bloc/opportunity_bloc.dart';
 
-TableData getTableData(Bloc bloc, String classificationId, BuildContext context,
-    Opportunity item, int index,
-    {dynamic extra}) {
+TableData getTableData(
+  Bloc bloc,
+  String classificationId,
+  BuildContext context,
+  Opportunity item,
+  int index, {
+  dynamic extra,
+}) {
   final localizations = MarketingLocalizations.of(context)!;
   List<TableRowContent> rowContent = [];
   bool isPhone = isAPhone(context);
   if (isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrShortId,
         width: 15,
         value: CircleAvatar(
@@ -38,82 +44,125 @@ TableData getTableData(Bloc bloc, String classificationId, BuildContext context,
             item.pseudoId.lastChar(3),
             style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
           ),
-        )));
+        ),
+      ),
+    );
   }
-  rowContent.add(TableRowContent(
+  rowContent.add(
+    TableRowContent(
       name: localizations.tableHdrId,
       width: isPhone ? 15 : 10,
-      value: Text(item.pseudoId, key: Key('id$index'))));
-  rowContent.add(TableRowContent(
+      value: Text(item.pseudoId, key: Key('id$index')),
+    ),
+  );
+  rowContent.add(
+    TableRowContent(
       name: localizations.tableHdrName,
       width: isPhone ? 35 : 15,
-      value: Text("${item.opportunityName}", key: Key('name$index'))));
+      value: Text("${item.opportunityName}", key: Key('name$index')),
+    ),
+  );
   if (!isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrAmount,
         width: 5,
-        value: Text(item.estAmount.toString(),
-            key: Key('estAmount$index'), textAlign: TextAlign.center)));
+        value: Text(
+          item.estAmount.toString(),
+          key: Key('estAmount$index'),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
   if (!isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrProbability,
         width: 5,
-        value: Text(item.estProbability.toString(),
-            key: Key('estProbability$index'), textAlign: TextAlign.center)));
+        value: Text(
+          item.estProbability.toString(),
+          key: Key('estProbability$index'),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
   if (!isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrLead,
         width: 15,
         value: Text(
           (item.leadUser != null
               ? "${item.leadUser!.firstName} "
-                  "${item.leadUser!.lastName},\n "
-                  "${item.leadUser!.company!.name}"
+                    "${item.leadUser!.lastName},\n "
+                    "${item.leadUser!.company!.name}"
               : ""),
           key: Key('lead$index'),
-        )));
+        ),
+      ),
+    );
   }
   if (!isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrLeadEmail,
         width: 10,
         value: Text(
           item.leadUser != null ? item.leadUser!.email ?? '' : "",
           key: Key('leadEmail$index'),
-        )));
+        ),
+      ),
+    );
   }
   if (!isPhone) {
-    rowContent.add(TableRowContent(
+    rowContent.add(
+      TableRowContent(
         name: localizations.tableHdrStage,
         width: 8,
-        value: Text("${item.stageId}",
-            key: Key('stageId$index'), textAlign: TextAlign.center)));
+        value: Text(
+          "${item.stageId}",
+          key: Key('stageId$index'),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
-  rowContent.add(TableRowContent(
+  rowContent.add(
+    TableRowContent(
       name: localizations.tableHdrNextStep,
       width: 17,
-      value: Text(item.nextStep != null ? "${item.nextStep}" : "",
-          key: Key('nextStep$index'), textAlign: TextAlign.center)));
-  rowContent.add(TableRowContent(
+      value: Text(
+        item.nextStep != null ? "${item.nextStep}" : "",
+        key: Key('nextStep$index'),
+        textAlign: TextAlign.center,
+      ),
+    ),
+  );
+  rowContent.add(
+    TableRowContent(
       width: isPhone ? 5 : 5,
       name: localizations.tableHdrDelete,
       value: IconButton(
-          visualDensity: VisualDensity.compact,
-          padding: EdgeInsets.zero,
-          key: Key('delete$index'),
-          icon: const Icon(Icons.delete_forever),
-          tooltip: localizations.removeItemTooltip,
-          onPressed: () async {
-            bool? result = await confirmDialog(
-                context,
-                localizations.deleteItemConfirm(item.pseudoId),
-                localizations.cannotBeUndone);
-            if (result == true) {
-              bloc.add(OpportunityDelete(item));
-            }
-          })));
+        visualDensity: VisualDensity.compact,
+        padding: EdgeInsets.zero,
+        key: Key('delete$index'),
+        icon: const Icon(Icons.delete_forever),
+        tooltip: localizations.removeItemTooltip,
+        onPressed: () async {
+          bool? result = await confirmDialog(
+            context,
+            localizations.deleteItemConfirm(item.pseudoId),
+            localizations.cannotBeUndone,
+          );
+          if (result == true) {
+            bloc.add(OpportunityDelete(item));
+          }
+        },
+      ),
+    ),
+  );
 
   return TableData(rowHeight: isPhone ? 40 : 40, rowContent: rowContent);
 }
