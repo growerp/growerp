35802ee01687f2caf83b3b7894f3326996fccec3
Hans Bakker <h.bakker@antwebsystems.com>
2025-09-29 12:28:43 +0700

    feat: opportunity translations

--- flutter/packages/growerp_marketing/lib/growerp_marketing.dart
+++ flutter/packages/growerp_marketing/lib/growerp_marketing.dart
@@ -2,5 +2,5 @@ export 'src/opportunities/views/views.dart';
 export 'src/opportunities/bloc/opportunity_bloc.dart';
 export 'src/opportunities/integration_test/marketing_test_model.dart';
 export 'src/opportunities/integration_test/opportunity_test.dart';
-export 'l10n/generated/marketing_localizations.dart';
+export 'package:growerp_marketing/l10n/generated/marketing_localizations.dart';
 export 'src/get_marketing_bloc_providers.dart';
