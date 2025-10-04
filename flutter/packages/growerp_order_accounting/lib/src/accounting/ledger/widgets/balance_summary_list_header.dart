ab82935473a4055c07ae4e32a9784dca7e0372a5
Hans Bakker <h.bakker@antwebsystems.com>
2025-10-04 10:27:28 +0700

    fix: remove l10n app files

--- flutter/packages/growerp_order_accounting/lib/src/accounting/ledger/widgets/balance_summary_list_header.dart
+++ flutter/packages/growerp_order_accounting/lib/src/accounting/ledger/widgets/balance_summary_list_header.dart
@@ -20,8 +20,11 @@ import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class BalanceSummaryListHeader extends StatefulWidget {
   const BalanceSummaryListHeader(
-      this.controller, this.ledgerReport, this.isPhone,
-      {super.key});
+    this.controller,
+    this.ledgerReport,
+    this.isPhone, {
+    super.key,
+  });
   final ItemScrollController controller;
   final LedgerReport ledgerReport;
   final bool isPhone;
@@ -37,66 +40,85 @@ class _BalanceSummaryListHeaderState extends State<BalanceSummaryListHeader> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        leading: GestureDetector(
-            key: const Key('search'),
-            onTap: (() => setState(() => search = !search)),
-            child: const Icon(Icons.search_sharp, size: 40)),
-        title: search
-            ? Row(children: <Widget>[
+      leading: GestureDetector(
+        key: const Key('search'),
+        onTap: (() => setState(() => search = !search)),
+        child: const Icon(Icons.search_sharp, size: 40),
+      ),
+      title: search
+          ? Row(
+              children: <Widget>[
                 Expanded(
-                    child: TextField(
-                  key: const Key('searchField'),
-                  autofocus: true,
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
+                  child: TextField(
+                    key: const Key('searchField'),
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      hintText: "search in account code and name...",
                     ),
-                    hintText: "search in account code and name...",
+                    onChanged: ((value) =>
+                        setState(() => searchString = value)),
                   ),
-                  onChanged: ((value) => setState(() => searchString = value)),
-                )),
+                ),
                 OutlinedButton(
-                    key: const Key('searchButton'),
-                    child: const Text('Search'),
-                    onPressed: () async {
-                      int index = 0;
-                      for (var el in widget.ledgerReport.glAccounts) {
-                        if (el.accountCode!.contains(searchString) ||
-                            el.accountName!.contains(searchString)) {
-                          break;
-                        }
-                        index++;
+                  key: const Key('searchButton'),
+                  child: const Text('Search'),
+                  onPressed: () async {
+                    int index = 0;
+                    for (var el in widget.ledgerReport.glAccounts) {
+                      if (el.accountCode!.contains(searchString) ||
+                          el.accountName!.contains(searchString)) {
+                        break;
                       }
-                      widget.controller.scrollTo(
-                          index: index,
-                          duration: const Duration(seconds: 1),
-                          curve: Curves.linear);
-                    })
-              ])
-            : Column(children: [
+                      index++;
+                    }
+                    widget.controller.scrollTo(
+                      index: index,
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.linear,
+                    );
+                  },
+                ),
+              ],
+            )
+          : Column(
+              children: [
                 if (widget.isPhone)
                   const Text("Account Name", textAlign: TextAlign.center),
-                Row(children: <Widget>[
-                  const Expanded(
-                      child: Text("Code", textAlign: TextAlign.center)),
-                  if (!widget.isPhone)
+                Row(
+                  children: <Widget>[
                     const Expanded(
-                        child: Text("Name", textAlign: TextAlign.center)),
-                  const Expanded(
-                      child: Text("Begin", textAlign: TextAlign.right)),
-                  if (!widget.isPhone)
+                      child: Text("Code", textAlign: TextAlign.center),
+                    ),
+                    if (!widget.isPhone)
+                      const Expanded(
+                        child: Text("Name", textAlign: TextAlign.center),
+                      ),
                     const Expanded(
-                        child:
-                            Text("Post.\nDebit", textAlign: TextAlign.right)),
-                  if (!widget.isPhone)
+                      child: Text("Begin", textAlign: TextAlign.right),
+                    ),
+                    if (!widget.isPhone)
+                      const Expanded(
+                        child: Text("Post.\nDebit", textAlign: TextAlign.right),
+                      ),
+                    if (!widget.isPhone)
+                      const Expanded(
+                        child: Text(
+                          "Post.\nCredit",
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
                     const Expanded(
-                        child:
-                            Text("Post.\nCredt", textAlign: TextAlign.right)),
-                  const Expanded(
-                      child: Text("End Bal.", textAlign: TextAlign.right)),
-                ]),
+                      child: Text("End Bal.", textAlign: TextAlign.right),
+                    ),
+                  ],
+                ),
                 const Divider(),
-              ]),
-        trailing: const Text(' '));
+              ],
+            ),
+      trailing: const Text(' '),
+    );
   }
 }
