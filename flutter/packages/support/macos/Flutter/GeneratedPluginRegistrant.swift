08103d59a23fc7d02cbc636ce244b800ccc53bdc
Hans Bakker <h.bakker@antwebsystems.com>
2026-02-08 09:24:01 +0700

    feat: Adopt Flutter workspaces and update package dependencies across various packages.

--- flutter/packages/support/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/packages/support/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import file_picker
 import file_selector_macos
 import package_info_plus
-import path_provider_foundation
 import printing
 import shared_preferences_foundation
 import url_launcher_macos
@@ -17,7 +16,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
