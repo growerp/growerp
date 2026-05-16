b5b551e2028d366a692da3246f11a685c84929b3
hansbak <h.bakker@antwebsystems.com>
2026-05-16 08:28:15 +0700

    fix: update video player plugin reference

--- flutter/packages/growerp_catalog/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/packages/growerp_catalog/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,5 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
